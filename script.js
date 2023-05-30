32ad9eb1270aeba6d39f42422efb6f9952488288
facubrujo <128422200+facubrujo@users.noreply.github.com>
2023-05-30 15:19:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 var boton1 = document.getElementById("boton1");
+let boton2 = document.getElementById("boton2");
+let boton3 = document.getElementById("boton3");
 
 boton1.addEventListener("click", function(boton1){
 
@@ -22,4 +24,29 @@ boton1.addEventListener("click", function(boton1){
         console.log(men);
     });
 
+});
+
+boton2.addEventListener("click",function(){
+    let num1 = parseInt(prompt("Ingrese un numero")); 
+    let num2 = parseInt(prompt("Ingrese un segundo numero")); 
+    let suma = (num1 + num2);
+    let resta = (num1 - num2);
+    let div = (num1 / num2);
+    let multi = (num1 * num2);
+
+    var objeto = {suma,resta,div,multi};
+
+    console.log("la suma de "+ num1 +" + "+ num2 +" = "+objeto.suma);
+    console.log("la resta de "+ num1 +" - "+ num2 +" = "+objeto.resta);
+    console.log("la division de " +num1 +" / "+ num2 +" = "+objeto.div);
+    console.log("el producto de "+ num1 +" * "+ num2 +" = "+objeto.multi);
+});
+
+boton3.addEventListener("click",function(){
+    let invertir = prompt("Ingrese una palabra o frase para invertirla");
+    function invertirFrase ( invertir){
+    let palabraInvertida = invertir.split("").reverse().join("");
+    return document.write(palabraInvertida);
+}
+//invertirFrase(invertir);
 });
